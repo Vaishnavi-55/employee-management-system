27d58b70b6ade8175ff6d2b69da29d695ae98967
vaishnavi <borravaishnavi6@gmail.com>
2025-12-30 15:29:44 +0530

    Add Django REST Framework and api app

--- djangoprojects/settings.py
+++ djangoprojects/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'DBAPP',
     'EmployeeApp',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- djangoprojects/urls.py
+++ djangoprojects/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee/',include('DBAPP.urls')),
-    path('EmpApp/',include('EmployeeApp.urls'))
+    path('EmpApp/',include('EmployeeApp.urls')),
+    path('api/',include('api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
